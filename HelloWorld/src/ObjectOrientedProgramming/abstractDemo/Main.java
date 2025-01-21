b49c2058defc03517b5015ac84bcdc7543109ddb
nihad <nihad.m@captainfresh.in>
2025-01-22 00:10:47 +0530

    commit

---
+++ HelloWorld/src/ObjectOrientedProgramming/abstractDemo/Main.java
@@ -0,0 +1,8 @@
+package ObjectOrientedProgramming.abstractDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        Son son = new Son(24);
+        son.career("coder");
+    }
+}
